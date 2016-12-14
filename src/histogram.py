ca53aa2064aabd9b9888ec729b029f532155e490
rahulkrverma <rahulkrverma@hotmail.com>
2016-12-14 16:53:42 +0530

    histo

--- src/histogram.py
+++ src/histogram.py
@@ -1,36 +1,48 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cPickle
 import os
+import cv2
+import pandas as pd
 
-def cal_hist(img_arr):
-	
-	img_arr = img_arr.mean(axis=2).flatten()
-	img_b, img_bins, img_patches = plt.hist(img_arr, 255)
+def calc_hist(img_arr):
 
-	return img_bins
+	rows, cols = img_arr.shape[:2]
+	basehsv = cv2.cvtColor(img_arr,cv2.COLOR_BGR2HSV)
 
-def save_bins(img_bins, profile_id):
-	file_name = '../database/histogram/' + str(profile_id) + '.pkl'
+	hbins = 180
+	sbins = 255
+	hrange = [0,180]
+	srange = [0,256]
+	ranges = hrange+srange
+
+	img_hist = cv2.calcHist(basehsv,[0,1],None,[180,256],ranges)
+	cv2.normalize(img_hist,img_hist,0,255,cv2.NORM_MINMAX)
+
+	return img_hist
+
+def save_hist(img_hist, profile_id):
+	file_name = '../DataBase/histogram/' + str(profile_id) + '.pkl'
+
 	f = open(file_name, 'wb')
-	cPickle.dump(vars, f, protocol=cPickle.HIGHEST_PROTOCOL)
+	cPickle.dump(img_hist, f, protocol=cPickle.HIGHEST_PROTOCOL)
 	f.close()
 
 def search_hist(profile_id, img_arr):
-	hist_path = '../database/histogram/'
-	img_bins = cal_hist(img_arr)
+	hist_path = '../DataBase/histogram/'
+	img_hist = calc_hist(img_arr)
 	response = {}
 	for x in profile_id:
 		file_path = os.path.join(hist_path, x)
+		file_path = file_path + '.pkl'
 		data = open(file_path, 'rb')
-		bins = cPickle.load(data)
-		distance = img_bins - bins
-		distance = format(np.dot(distance, distance))
+		hist = cPickle.load(data)
+
+		distance = cv2.compareHist(img_hist,hist,0)
+		print distance, file_path
 		response[x] = distance
+	print response
 
 
-
-
-def histo(img_arr, profile_id):
-	img_bins = cal_hist(img_arr)
-	save_bins(img_bins, profile_id)
+def add_hist(img_arr, profile_id):
+	img_hist = calc_hist(img_arr)
+	save_hist(img_hist, profile_id)
